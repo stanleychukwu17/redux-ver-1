04939151a837e96d462043dc6b077f59abb31047
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-23 19:34:08 +0100

    changes

---
+++ src/components/CakeComp.js
@@ -0,0 +1,10 @@
+const CakeComp = () => {
+    return (
+        <div>
+            <div>Cakes we have now - 10</div>
+            <div><button>Buy cake</button></div>
+        </div>
+    );
+}
+ 
+export default CakeComp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import CakeComp from './components/CakeComp.js';
 
 const App = () => {
   return (
-    <div>Welcome back to react building</div>
+    <div className="home">
+      <div>Welcome back to react building </div>
+      <div><CakeComp /></div>
+    </div>
   );
 }
  
