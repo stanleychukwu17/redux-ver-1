3c72a71780392c9106283d156ae675e03795f2b9
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-24 20:28:35 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
-import {useEffect} from 'react';
-import CakeComp from './components/CakeComp.js';
-import {connect} from 'react-redux'
 
 const App = (props) => {
-    useEffect(() => {
-        console.log('rendered from app')
-    })
 
-    return (
-        <div className="home">
-            <div>Welcome back to react building - {props.numCakes} </div>
-            <div><CakeComp /></div>
-        </div>
-    );
 }
-  
-const mapStateToProps = (state) => {
-return {numCakes: state.numCakes}
-}
-export default connect(mapStateToProps)(App);
+
+export default App;
 // export default App;
