30eeb40cf928367862b41646935a0733b4023271
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-25 15:18:36 +0100

    changes

--- src/components/ItemComp.js
+++ src/components/ItemComp.js
@@ -1,18 +1,30 @@
 import {useState} from 'react';
+import {connect} from 'react-redux';
+import {buyCake, buyCream} from '../redux/actions'
 
 const ItemComp = () => {
     let [qty, setQty] = useState(1);
+    let [item, setItem] = useState('cake');
 
     return (
         <div>
             <div>Enter num of items to buy</div>
             <div>
-                <select><option value="">Buy cake</option><option value="">Buy creaam</option></select>
-                <input type="num" value={qty} />
-                <button>Buy now</button>
+                <select value={item} onChange={(e) => setItem(e.target.value)}>
+                    <option value="cake">Buy cake</option><option value="cream">Buy creaam</option>
+                </select>
+                <input type="num" value={qty} onChange={(e) => setQty(e.target.value)} />
+                <button>Buy {qty} {item} now</button>
             </div>
         </div>
     )
 }
- 
-export default ItemComp;
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        'buyCake':() => dispatch(buyCake()),
+        'buyCream':() => dispatch(buyCream()),
+    }
+}
+
+export default connect(null, mapDispatchToProps)(ItemComp);
