23125b37c19f7f4f601be299e9b60a9b4a249ab2
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-25 15:05:11 +0100

    changes

---
+++ src/components/ItemComp.js
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+
+const ItemComp = () => {
+    let [qty, setQty] = useState(1);
+
+    return (
+        <div>
+            <div>Enter num of items to buy</div>
+            <div>
+                <select><option value="">Buy cake</option><option value="">Buy creaam</option></select>
+                <input type="num" value="{qty}" />
+                <button>Buy now</button>
+            </div>
+        </div>
+    )
+}
+ 
+export default ItemComp;
