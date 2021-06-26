f92e46a16b73fbc3163a62ad7b53a9be5e2a5d44
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-26 11:47:11 +0100

    changes

--- src/redux/cakeReducer.js
+++ src/redux/cakeReducer.js
@@ -1,12 +1,14 @@
-const cur_state = {'numCakes':30};
+const cur_state = {
+    'numCakes':30, 'u_all':{'loading':true, 'users':[], 'error':null}
+};
 const cream_state = {'numCreams':60}
 
 export let cakeReducer = (state = cur_state, action) => {
     switch (action.type) {
         case 'BUY_CAKE': return {...state, 'numCakes': state.numCakes - action.payload}
-        case '' : return {...state, ''}
-        case '' : return {...state, ''}
-        case '' : return {...state, ''}
+        case 'FETCH_USERS_LOADING' : return {...state, 'u_all':{...state.u_all, 'loading':true, 'error':null}}
+        case 'FETCH_USERS_SUCCESS' : return {...state, 'u_all':{...state.u_all, 'loading':false, 'error':null}}
+        case 'FETCH_USERS_ERROR' : return {...state, 'u_all':{...state.u_all, 'loading':false, 'error':action.payload}}
         default: return state
     }
 }
