84155cbdafcb883994c657e1ed04d3edc33e7f1f
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-26 11:18:35 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import CakeContainer from "./components/CakeComp";
 import CreamContainer from './components/CreamComp';
 import ItemComp from './components/ItemComp';
+import UserComp from './components/UserComp';
 
 const App = (props) => {
     return (
@@ -8,6 +9,7 @@ const App = (props) => {
             <div className="holder"><CakeContainer /></div>
             <div className="holder"><CreamContainer /></div>
             <div className="holder"><ItemComp /></div>
+            <div className="holder hld_user"><UserComp /></div>
         </div>
     );
 }
---
+++ src/components/UserComp.js
@@ -0,0 +1,9 @@
+const UserComp = (props) => {
+    return (
+        <div>
+            <div>For users we're going to load</div>
+        </div>
+    );
+}
+ 
+export default UserComp;
