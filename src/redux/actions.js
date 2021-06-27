cfd045dc911e36f8145c819b352d359292adae68
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-27 10:50:54 +0100

    changes

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -11,3 +11,9 @@ export let fetch_users_preloading = () => ({'type':'FETCH_USERS_LOADING'})
 export let fetch_users_success = (users) => ({'type':'FETCH_USERS_SUCCESS', payload: users})
 
 export let fetch_users_error = (err_msg) => ({'type':'FETCH_USERS_ERROR', payload: err_msg})
+
+export let fetch_all_users = () => {
+    return function (dispatch) {
+        console.log('we go fetch them now');
+    }
+}
