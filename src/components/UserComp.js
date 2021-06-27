4543336812d7b6bede1a4f0077b9da2d8a2d7aa4
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-27 10:49:27 +0100

    changes

--- src/components/UserComp.js
+++ src/components/UserComp.js
@@ -1,7 +1,21 @@
+
+
+/*
+? tell us why the user is still loading 
+? write the function for fetching the users
+*/
+
 const UserComp = (props) => {
+    function Loading () {
+        return <div>Loading...</div>
+    }
+
     return (
         <div>
             <div>For users we're going to load</div>
+            <div>
+                <Loading />
+            </div>
         </div>
     );
 }
