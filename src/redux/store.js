8408023bf57781e393de0c4b59e367d96ef227d0
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-27 10:54:24 +0100

    changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk'
 
 import {cakeReducer, creamReducer } from "./cakeReducer";
@@ -8,5 +8,5 @@ import {cakeReducer, creamReducer } from "./cakeReducer";
 let rootReducer = combineReducers({
     'cake':cakeReducer, 'cream':creamReducer
 });
-let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
